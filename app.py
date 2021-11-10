75a7747f683c44741b016e9f500e59eb491d87a5
Slaktish1 <edvin.lolic@gmail.com>
2021-11-10 21:26:54 +0100

    Update

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ db = SQLAlchemy(app)
 if __name__ == '__main__':
    app.run()
 
-class User(db.Model):
+class UserInformation(db.Model):
    id = db.Column(db.Integer, primary_key=True)
    dbname = db.Column(db.String(80), unique=True, nullable=False)
    dbStreet = db.Column(db.String(120), nullable=False)
@@ -17,6 +17,22 @@ class User(db.Model):
    dbCountry = db.Column(db.String(120), nullable=False)
    dbEmail = db.Column(db.String(120), nullable=False)
    dbPw = db.Column(db.String(120), nullable=False)
+
+class ProductAsset(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   prodName = db.Column(db.String(80), unique=True, nullable=False)
+   prodPrice = db.Column(db.Float, unique=False, nullable=False)
+   prodImage = db.Column(db.LargeBinary, unique=False, nullable=False)
+   prodColor = db.Column(db.String(120), unique=False, nullable=False)
+
+class ActiveOrder(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   dbname = db.Column(db.String(80), unique=False, nullable=False)
+   dbLastname = db.Column(db.String(80), unique=False, nullable=False)
+   dbStreet = db.Column(db.String(120), nullable=False)
+   dbCity = db.Column(db.String(120), nullable=False)
+   dbCountry = db.Column(db.String(120), nullable=False)
+   dbUserID = db.Column(db.String(120), nullable=True)
 
 @app.route('/')
 def HomePage():
